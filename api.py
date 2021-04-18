8b61b2f7c76359b2c572f6ec6e74dfeeb7c7e1bc
wwwshwww <www.shinderu.www@gmail.com>
2021-04-19 03:37:43 +0900

    React

--- api.py
+++ api.py
@@ -10,38 +10,33 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
-    if request.method == 'POST':
-        response = {}
-        stream = request.files['image'].stream
-        img_arr = np.asarray(bytearray(stream.read()), dtype=np.uint8)
+    response = {'num': 0}
+    stream = request.files['image'].stream
+    img_arr = np.asarray(bytearray(stream.read()), dtype=np.uint8)
 
-        
-        vthresh = 210
-        eps = 0.99
-        size = 4
-        movable = 1.5
-        std = 0.15
-        is_stride = True
-        leaves = 20
-        fps = 30
+    vthresh = 210
+    eps = 0.99
+    size = 4
+    movable = 1.5
+    std = 0.15
+    is_stride = True
+    leaves = 20
+    fps = 30
 
-        imgs = None
-        abort = False
+    imgs = None
+    abort = False
 
-        if len(img_arr) != 0:
-            try:
-                imgs = png2b64(create_images(img_arr, leaves, vthresh, eps, size, movable, std, is_stride))
-            except:
-                abort = True
-
-        if not abort:
-            for i, img in enumerate(imgs):
-                response[f'image_{i}'] = img
-
-        response['abort'] = abort
-        response['vthresh'] = 
+    if len(img_arr) != 0:
+        try:
+            imgs = png2b64(create_images(img_arr, leaves, vthresh, eps, size, movable, std, is_stride))
+            response['num'] = len(imgs)
+            response['images'] = imgs
+            
+        except:
+            response['num'] = 0
+            abort = True
     
-    return temp
+    return jsonify(**response)
 
 if __name__ == "__main__":
     # app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
--- react-app/react-sample/src/App.js
+++ react-app/react-sample/src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const SIZE_LIMIT = (1024 ** 2) * 8
+
+class Uploader extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {file: ''};
+    this.fileInput = React.createRef();
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(event){
+    event.preventDefault();
+    let file = event.target.files[0]
+    if (event.target.files.length === 0){ return; }
+    if (file.size > SIZE_LIMIT){
+      alert(
+        `Over the file upload size limit - '${file.name}'`
+      );
+      this.setState({value: '', previewUrl: ''})
+    }else{
+      let reader = new FileReader()
+      reader.onloadend = () => {
+        this.setState({file: file, previewUrl: reader.result});
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          Image:<br />
+          <input 
+            type='file' 
+            id='input' 
+            accept="image/png, image/jpeg" 
+            value={this.state.value} 
+            ref={this.fileInput} 
+            onChange={this.handleChange} 
+          />
+          <br/>
+          <img src={this.state.previewUrl} className='preview' />
+        </label>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Whoaaaaaaaaaaaaaaaa!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Uploader />
       </header>
     </div>
   );
